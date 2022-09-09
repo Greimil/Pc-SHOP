2f33a2fe702ec2a7c88497e1d83b7f84ab38ddbb
Greimil <greimilnunez@gmail.com>
2022-09-09 14:44:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("menuicon")
+const ulMenu = document.getElementById("ulOculto")
+
+
+
+
+menuIcon.addEventListener("click", (e)=> {
+
+    ulMenu.classList.toggle("show")
+    
+
+})
